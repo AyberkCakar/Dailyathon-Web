8b87de6ad4839f8436c715f059137e14c60765eb
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-18 17:59:26 +0300

    feat: added sidebar navigation

--- src/app/config/page-menus.ts
+++ src/app/config/page-menus.ts
@@ -1,12 +1,62 @@
 var pageMenus = [{
-  'icon': 'fa fa-th-large',
-  'title': 'Home',
-  'url': '/home'
-},{
-  'icon': 'fa fa-align-left',
+  'icon': 'fab fa-windows',
   'title': 'Dashboard',
-  'url': '',
-  'submenu': []
+  'url': '/dashboard'
+},{
+  'icon': 'fas fa-user-secret',
+  'title': 'Admin',
+  'url': '/admin'
+},{
+  'icon': 'fas fa-users',
+  'title': 'User',
+  'url': '/user'
+},{
+  'icon': 'far fa-chart-bar',
+  'title': 'İstatistik',
+  'url': '/istatistik'
+},{
+  'icon': 'fas fa-th-list',
+  'title': 'Category',
+  'url': '/category'
+},{
+  'icon': 'fas fa-hashtag',
+  'title': 'Tag',
+  'url': '/tag'
+},{
+  'icon': 'fas fa-bullhorn',
+  'title': 'Duyuru',
+  'url': '/duyuru'
+},{
+  'icon': 'fas fa-tasks',
+  'title': 'Anket',
+  'url': '/anket'
+},{
+  'icon': 'fas fa-futbol',
+  'title': 'Spor',
+  'url': '/spor'
+},{
+  'icon': 'far fa-smile',
+  'title': 'Etkinlik',
+  'url': '/etkinlik'
+},{
+  'icon': 'fas fa-newspaper',
+  'title': 'Haber',
+  'url': '/haber'
+},{
+  'icon': 'far fa-file-code',
+  'title': 'Log',
+  'caret': 'true',
+  'submenu': [{
+    'url': '/servelog',
+    'title': 'Serve Log List',
+  },{
+    'url': '/databotlog',
+    'title': 'Databot Log List'
+  }]
+},{
+  'icon': 'fas fa-code',
+  'title': 'Admin Log List',
+  'url': '/home'
 }];
 
 export default pageMenus;
