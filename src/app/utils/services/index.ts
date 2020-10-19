6e2dca623a0fec28c1a9029d6ee23b8144e37cf8
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-19 20:34:54 +0300

    feat: added tag list page

--- src/app/utils/services/index.ts
+++ src/app/utils/services/index.ts
@@ -2,3 +2,4 @@ export * from './api-fetch/api-fetch.service';
 export * from './auth/auth.service';
 export * from './user/user.service';
 export * from './admin/admin.service';
+export * from './tag/tag.service';
