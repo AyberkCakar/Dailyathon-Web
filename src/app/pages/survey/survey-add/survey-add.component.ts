97ca927b8483a98e09d16e6d99d2a331756be35c
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-20 23:47:33 +0300

    feat: added survey insert api connection

--- src/app/pages/survey/survey-add/survey-add.component.ts
+++ src/app/pages/survey/survey-add/survey-add.component.ts
@@ -14,6 +14,20 @@ export class SurveyAddComponent {
   {
   }
 
+  async surveyAdd(surveyname:string,tablename:string,c1:Date,c2:Date,url:string)
+  {
+    this.model.SurveyName = surveyname;
+    this.model.SurveyTableName = tablename;
+    this.model.SurveyStartDate = c1;
+    this.model.SurveyDueDate = c2;
+    this.model.SurveyUrl = url;
+    try {
+      await this._surveyService.insertAsync(this.model)
+      await this.router.navigateByUrl('/survey')
+    } catch (error) {
+
+    }
+  }
 
 }
 
