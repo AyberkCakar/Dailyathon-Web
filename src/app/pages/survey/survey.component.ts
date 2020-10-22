bbf269ffc6c986bf4564d6c5d0f72aa33016e6f1
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-22 19:05:51 +0300

    feat: added UI Modal for survey deletion

--- src/app/pages/survey/survey.component.ts
+++ src/app/pages/survey/survey.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {SurveyModel} from './survey.model';
 import { SurveyService } from '../../utils/services';
 import { Router } from '@angular/router';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'survey',
@@ -10,16 +11,16 @@ import { Router } from '@angular/router';
 
 export class SurveyComponent {
   model:Array<SurveyModel>;
-  constructor(private router: Router, private _surveyService: SurveyService )
-  {
-  }
+  survey: SurveyModel = new SurveyModel();
+  closeResult: string;
+  deleteID: number;
+  constructor(private router: Router, private _surveyService: SurveyService , private modalService: NgbModal )
+  {}
 
   async ngOnInit(){
     try {
       this.model = <Array<SurveyModel>>await this._surveyService.listAsync()
-
     } catch (error) {
-
     }
   };
 
@@ -28,5 +29,23 @@ export class SurveyComponent {
     this.router.navigateByUrl('/surveyAdd');
   }
 
+  open(content, ID) {
+    this.deleteID = ID;
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 }
 
