3e0445f67e65bc609989f4226f75186d112355e8
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-22 17:36:30 +0300

    feat: added UI Modal for league deletion

--- src/app/pages/league/league.component.ts
+++ src/app/pages/league/league.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {LeagueModel} from './league.model';
 import { LeagueService } from '../../utils/services';
 import { Router } from '@angular/router';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'league',
@@ -10,21 +11,40 @@ import { Router } from '@angular/router';
 
 export class LeagueComponent {
   model:Array<LeagueModel>;
-  constructor(private router: Router, private _leagueService: LeagueService )
-  {
-  }
+  league: LeagueModel = new LeagueModel();
+  closeResult: string;
+  deleteID: number;
+  constructor(private router: Router, private _leagueService: LeagueService , private modalService: NgbModal )
+  {}
 
   async ngOnInit(){
     try {
       this.model = <Array<LeagueModel>>await this._leagueService.listAsync()
-
     } catch (error) {
-
     }
   }
 
   goRouter()
   {
     this.router.navigateByUrl('/leagueAdd');
+  }
+
+  open(content, ID) {
+    this.deleteID = ID;
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 }
