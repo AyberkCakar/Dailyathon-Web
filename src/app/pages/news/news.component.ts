97c4d528d86a5be271ae890309180afd1b6c79d7
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-22 18:51:12 +0300

    feat: added UI Modal for news deletion

--- src/app/pages/news/news.component.ts
+++ src/app/pages/news/news.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {NewsModel} from './news.model';
 import {NewsService} from '../../utils/services';
 import { Router } from '@angular/router';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'news',
@@ -10,16 +11,36 @@ import { Router } from '@angular/router';
 
 export class NewsComponent {
   model:Array<NewsModel>;
-  constructor(private router: Router, private _newsService: NewsService )
+  news: NewsModel = new NewsModel();
+  closeResult: string;
+  deleteID: number;
+  constructor(private router: Router, private _newsService: NewsService , private modalService: NgbModal)
   {
   }
 
   async ngOnInit(){
     try {
-      this.model = <Array<NewsModel>>await this._newsService.listAsync()
-
+      this.model = <Array<NewsModel>>await this._newsService.listAsync();
     } catch (error) {
+    }
+  }
 
+  open(content, ID) {
+    this.deleteID = ID;
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
     }
   }
 }
