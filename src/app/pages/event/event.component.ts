06b26870c12dfd35bc653d5e751054fc15a49701
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-22 17:31:19 +0300

    feat: added UI Modal for event deletion

--- src/app/pages/event/event.component.ts
+++ src/app/pages/event/event.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {EventModel} from './event.model';
 import {EventService} from '../../utils/services';
 import { Router } from '@angular/router';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'event',
@@ -10,9 +11,11 @@ import { Router } from '@angular/router';
 
 export class EventComponent {
   model:Array<EventModel>;
-  constructor(private router: Router, private _eventService: EventService )
-  {
-  }
+  announcement: EventModel = new EventModel();
+  closeResult: string;
+  deleteID: number;
+  constructor(private router: Router, private _eventService: EventService , private modalService: NgbModal )
+  {}
 
   async ngOnInit(){
     try {
@@ -24,5 +27,24 @@ export class EventComponent {
   goRouter()
   {
     this.router.navigateByUrl('/eventAdd');
+  }
+
+  open(content, ID) {
+    this.deleteID = ID;
+    this.modalService.open(content).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
   }
 }
