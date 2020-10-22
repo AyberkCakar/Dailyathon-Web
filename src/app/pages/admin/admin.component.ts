b94fef43ca5c0142d383a17e2f20a9859495eadc
AyberkCakar <ayberkcakar35@gmail.com>
2020-10-22 17:18:36 +0300

    fix: admin delete removed

--- src/app/pages/admin/admin.component.ts
+++ src/app/pages/admin/admin.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import {AdminModel} from './admin.model';
 import { AdminService } from '../../utils/services';
 import { Router } from '@angular/router';
-import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
@@ -13,10 +12,8 @@ import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 export class AdminComponent {
 
   model:Array<AdminModel>;
-  admin: AdminModel = new AdminModel();
-  closeResult: string;
-  deleteID: number;
-  constructor(private router: Router, private _adminService: AdminService , private modalService: NgbModal)
+
+  constructor(private router: Router, private _adminService: AdminService)
   {}
 
   async ngOnInit(){
@@ -29,24 +26,5 @@ export class AdminComponent {
   goRouter()
   {
     this.router.navigateByUrl('/adminAdd');
-  }
-
-  open(content, ID) {
-    this.deleteID = ID;
-    this.modalService.open(content).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
-    }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-    });
-  }
-
-  private getDismissReason(reason: any): string {
-    if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-    } else {
-      return  `with: ${reason}`;
-    }
   }
 }
