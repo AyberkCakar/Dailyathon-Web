79bda24a388f0fd13f772b5a55144224446f58ff
AyberkCakar <ayberkcakar35@gmail.com>
2020-11-07 21:06:11 +0300

    feat: added user find api connection

--- src/app/utils/services/user/user.service.ts
+++ src/app/utils/services/user/user.service.ts
@@ -13,6 +13,10 @@ async listAsync() {
     return await this._apiFetchService.requestAsync('GET','user',null,true);
   }
 
+  async findAsync(values) {
+    return await this._apiFetchService.requestAsync('POST','find/user',values,true);
+  }
+
   async deleteAsync(values) {
     return await this._apiFetchService.requestAsync('DELETE','user-delete',values,true);
   }
