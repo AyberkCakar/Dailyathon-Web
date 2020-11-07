f3f4cf45b5926275704baafb68e2de4c8c2e74ff
AyberkCakar <ayberkcakar35@gmail.com>
2020-11-07 13:59:29 +0300

    feat: added tag add notification

--- src/app/pages/tag/tag-add/tag-add.component.ts
+++ src/app/pages/tag/tag-add/tag-add.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import {TagModel} from '../tag.model';
 import {CategoryModel} from '../../category/category.model';
-
+import {NotifierService} from 'angular-notifier';
 import { TagService, CategoryService } from '../../../utils/services';
 import { Router } from '@angular/router';
 
@@ -14,14 +14,18 @@ export class TagAddComponent {
 
   model: TagModel = new TagModel();
   category: Array<CategoryModel>;
-  constructor(private router: Router, private _tagService: TagService ,private _categoryService:CategoryService)
-  {
+  constructor(private router: Router , private notifier: NotifierService , private _tagService: TagService ,private _categoryService:CategoryService)
+  {}
+
+  public showNotification( type: string, message: string ): void {
+    this.notifier.notify( type, message )
   }
 
   async ngOnInit(){
     try {
       this.category = <Array<CategoryModel>>await this._categoryService.listAsync()
     } catch (error) {
+      this.showNotification( 'error', error.message )
     }
   }
   async tagAdd(tagname:string,categoryID:number)
@@ -32,7 +36,7 @@ export class TagAddComponent {
       await this._tagService.insertAsync(this.model)
       await this.router.navigateByUrl('/tag')
     } catch (error) {
-
+      this.showNotification( 'error', error.message )
     }
   }
 }
