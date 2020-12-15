d03746205e89f387832cf0998a076cab81cfaf07
AyberkCakar <ayberkcakar35@gmail.com>
2020-12-15 17:16:17 +0300

    fix: fix servelog page design

--- src/app/pages/log/servelog/servelog.model.ts
+++ src/app/pages/log/servelog/servelog.model.ts
@@ -2,6 +2,8 @@ export class ServelogModel{
   LogID: number;
   RouterName: string;
   RouterType: string;
+  StatusCode: number;
+  StatusMessage: string;
   Message: string;
   Exception: string;
   RegDate: Date;
